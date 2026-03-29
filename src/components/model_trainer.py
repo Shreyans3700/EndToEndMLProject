eda4c3d71e2e273cf3d58f7da91cf1e220a7ab5e
Shreyans3700 <pandeyshreyansh46@gmail.com>
2026-03-30 01:21:26 +0530

    Model training.

---
+++ src/components/model_trainer.py
@@ -0,0 +1,74 @@
+import sys
+import os
+from sklearn.linear_model import LinearRegression, Ridge, Lasso
+from sklearn.svm import SVR
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from xgboost import XGBRegressor
+from src.logger import logging
+from src.exception import CustomException
+from dataclasses import dataclass
+from src.utils import evaluate_models
+from src.utils import save_object
+
+
+@dataclass
+class ModelTrainerConfig:
+    trained_model_file_path: str = os.path.join("artifacts", "model.pkl")
+
+
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config = ModelTrainerConfig()
+
+    def initiate_model_trainer(self, train_arr, test_arr):
+        try:
+            X_train, y_train, X_test, y_test = (
+                train_arr[:, :-1],  ## All rows and all cols till last one
+                train_arr[:, -1],  ## pick the last column from all rows
+                test_arr[:, :-1],
+                test_arr[:, -1],
+            )
+            models = {
+                "LinearRegression": LinearRegression(),
+                "Ridge": Ridge(),
+                "Lasso": Lasso(),
+                "Support Vector Regressor": SVR(),
+                "KN Regressor": KNeighborsRegressor(),
+                "DecisionTree Regressor": DecisionTreeRegressor(),
+                "RandomForest Regressor": RandomForestRegressor(),
+                "XG Boost": XGBRegressor(),
+            }
+
+            model_report: dict = evaluate_models(
+                X_train, y_train, X_test, y_test, models
+            )
+
+            logging.info("Model Reports are %s", model_report)
+
+            test_r2_scores = [
+                scr["Test Data Report"]["Testing R2 Score"]
+                for scr in model_report.values()
+            ]
+
+            best_model_score_index = test_r2_scores.index(max(sorted(test_r2_scores)))
+
+            best_model_name = list(model_report.keys())[best_model_score_index]
+
+            best_model = models[best_model_name]
+
+            best_score = test_r2_scores[best_model_score_index]
+
+            if best_score < 0.6:
+                logging.info("No best model found in both training and testing.")
+                raise CustomException("No Best Model Found")
+
+            logging.info(
+                f"Best model found: {best_model_name} with R2 score: {best_score}"
+            )
+
+            save_object(best_model, self.model_trainer_config.trained_model_file_path)
+
+        except Exception as e:
+            raise CustomException(e, sys)
--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,8 @@ from src.exception import CustomException
 import os
 import sys
 import dill
+import numpy as np
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
 
 def save_object(object, file_path: str) -> bool:
@@ -15,6 +17,54 @@ def save_object(object, file_path: str) -> bool:
             dill.dump(obj=object, file=file_obj)
 
         return True
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+def calculate_model_score(y, y_pred, prefix):
+    mae = mean_absolute_error(y, y_pred)
+    mse = mean_squared_error(y, y_pred)
+    rmse = np.sqrt(mse)
+    r2_scr = r2_score(y, y_pred)
+
+    return {
+        f"{prefix} Mean Absolute Error": mae,
+        f"{prefix} Mean Squared Error": mse,
+        f"{prefix} Root Mean Squared Error": rmse,
+        f"{prefix} R2 Score": r2_scr,
+    }
+
+
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    try:
+        report = {}
+
+        for i in range(len(list(models.values()))):
+            model = list(models.values())[i]
+            model_name = list(models.keys())[i]
+            logging.info("Starting Model Training of %s", model_name)
+
+            model.fit(X_train, y_train)
+
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            training_data_model_report = calculate_model_score(
+                y_train, y_train_pred, "Training"
+            )
+            testing_data_model_report = calculate_model_score(
+                y_test, y_test_pred, "Testing"
+            )
+
+            model_report = {
+                "Training Data Report": training_data_model_report,
+                "Test Data Report": testing_data_model_report,
+            }
+
+            report[list(models.keys())[i]] = model_report
+
+        return report
 
     except Exception as e:
         raise CustomException(e, sys)
